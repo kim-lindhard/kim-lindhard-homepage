7026b487230776f3ae937f0ee87d989b56b03840
kimlindhard <kim.lindhard@gmail.com>
2020-12-22 14:54:07 +0100

    add install script for git prevent commit to master

---
+++ cool-stuff/install-are-you-sure-you-want-to-commit-to-main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Global installation instructions: (requires git 2.9 or later)
+# NOTE: if you configure core.hooksPath, then git only runs the hooks from that directory (and ignores repo-specific hooks in .git/hooks/), but these pre-commit hooks contain a block at the end which executes a repo-specific pre-commit hook if it's present. It's a small hax that I think is pretty nice.
+mkdir $HOME/.githooks
+
+curl -fL -o $HOME/.githooks/pre-commit https://raw.githubusercontent.com/kim-lindhard/kim-lindhard-homepage/master/cool-stuff/are-you-sure-you-want-to-commit-to-main.sh
+chmod +x $HOME/.githooks/pre-commit
+
+git config --global core.hooksPath $HOME/.githooks
